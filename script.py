9d9f33ac1064db8d75fa31cfdb0c49007ca709f2
aryan5610 <67310521+aryan5610@users.noreply.github.com>
2021-03-18 21:22:36 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,14 +2,8 @@ class script(object):
 
 
     START_MSG = """ <b>Hi {}
-
-You can call this as an Auto Filter Bot if you like :D
-
-This is Version 2 of Auto Filter Bot
-
-Bot gives button link to files in connected channels on query !
-
-No need to add filters for your files or movies from now on!
+this is anime search bot :D ..!! Exclusively to download anime from all telegram !! 
+Search all anime at one place @animerivs !! Join our group and search for anime there and you could download from whole telegram!'
 
 For more click <i>help</i></b>"""
 
@@ -53,17 +47,15 @@ No need add each filter again!
 Bot will automatically search for your files and give links to that!
 
 
-<b>© @TroJanzHEX</b>"""
+<b>© @Anime_Volume</b>"""
 
 
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
+    ABOUT_MSG = """⭕️<b>My Name : ANIME SEARCH BOT</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @anime_volume
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
 
 """
